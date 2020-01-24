b00f24c664bbf52c692e2f4e932d05d2c5c9861b
Mae Kangsadan <maemeier@live.com>
2020-01-24 20:34:41 +0100

    onSubmit

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,13 @@ import SeachBar from "./SearchBar";
 import "../styles.css";
 
 class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
   render() {
     return (
       <div className="ui container" style={{ marginTop: "20px" }}>
-        <SeachBar />
+        <SeachBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
